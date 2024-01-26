f8605b1763cc5d1745d88348903195441f03addb
Anastasija <anastasija.krstanoska@students.finki.ukim.mk>
2024-01-26 09:43:11 +0100

    Initial commit

---
+++ database/migrations/2024_01_24_213149_create_expenses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('name');
+            $table->float('price');
+            $table->unsignedBigInteger('user_id'); // Foreign key za korisnik
+            $table->unsignedBigInteger('category_id'); // Foreign key za kategorija
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users');  //ref
+            $table->foreign('category_id')->references('id')->on('categories');  //ref
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
